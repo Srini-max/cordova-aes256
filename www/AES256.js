9da97e0198414d5c521f3871cf0781572c1caf4f
Srini-max <54628670+Srini-max@users.noreply.github.com>
2020-02-09 10:20:12 +0400

    Update AES256.js

--- www/AES256.js
+++ www/AES256.js
@@ -1,30 +1,31 @@
 var exec = require('cordova/exec');
 
-var AES256 = function () {};
+var AES256 = {
 
-AES256.prototype.encrypt = function (secureKey, iv, value, success, error) {
-    if (secureKey && iv && value) {
-        exec(success, error, 'AES256', 'encrypt', [secureKey, iv, value]);
-    } else {
-        success('');
+	encrypt : function (secureKey, iv, value, success, error) {
+		if (secureKey && iv && value) {
+			exec(success, error, 'AES256', 'encrypt', [secureKey, iv, value]);
+		} else {
+			success('');
+		}
+	},
+
+	decrypt : function (secureKey, iv, value, success, error) {
+		if (secureKey && iv && value) {
+			exec(success, error, 'AES256', 'decrypt', [secureKey, iv, value]);
+		} else {
+			success('');
+		}
+	},
+
+    generateSecureKey : function (password, success, error) {
+		exec(success, error, 'AES256', 'generateSecureKey', [password]);
+	}
+
+    generateSecureIV : function (password, success, error) {
+        exec(success, error, 'AES256', 'generateSecureIV', [password]);
     }
-};
 
-AES256.prototype.decrypt = function (secureKey, iv, value, success, error) {
-    if (secureKey && iv && value) {
-        exec(success, error, 'AES256', 'decrypt', [secureKey, iv, value]);
-    } else {
-        success('');
-    }
-};
-
-AES256.prototype.generateSecureKey = function (password, success, error) {
-    exec(success, error, 'AES256', 'generateSecureKey', [password]);
-};
-
-AES256.prototype.generateSecureIV = function (password, success, error) {
-    exec(success, error, 'AES256', 'generateSecureIV', [password]);
-};
-
+}
 
 module.exports = AES256;
